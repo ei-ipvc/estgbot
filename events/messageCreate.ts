555fe3c74d94fb20ab396c05c7737f281d1e2d35
Matthew Rodrigues <mattcapazz@gmail.com>
2024-06-07 00:54:57 +0100

    fix: ignore ribas emoji for react job

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -1,4 +1,4 @@
-import { Events } from 'discord.js';
+import { Events } from 'discord.js'
 
 module.exports = {
   name: Events.MessageCreate,
@@ -11,10 +11,12 @@ module.exports = {
       'máchine',
       'nosso lider',
     ]
+    const content = message.content.replace(
+      '<:ribeiro3:855147982110457877>',
+      '',
+    )
     if (
-      keywords.some((keyword) =>
-        message.content.toLowerCase().includes(keyword),
-      ) &&
+      keywords.some((word) => content.toLowerCase().includes(word)) &&
       Math.random() <= 0.326 // 32.5% chance of reacting
     ) {
       message.react('🇷')
