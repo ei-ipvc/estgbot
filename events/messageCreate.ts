a0918673ed83c8623d0d7626d63bba5b4f50195f
Matthew Rodrigues <matt@capaz.dev>
2024-06-15 15:30:24 +0100

    fix: ignore self authored msg for react job

--- events/messageCreate.ts
+++ events/messageCreate.ts
@@ -1,20 +1,28 @@
 import { Events } from 'discord.js'
 
+const keywords = [
+  'astronauta',
+  'ribeirinho',
+  'ribeiro',
+  'ribas',
+  'máchine',
+  'nosso lider',
+]
+
 module.exports = {
   name: Events.MessageCreate,
-  execute(message: { content: string; react: (arg0: string) => Promise<any> }) {
-    const keywords = [
-      'astronauta',
-      'ribeirinho',
-      'ribeiro',
-      'ribas',
-      'máchine',
-      'nosso lider',
-    ]
+  execute(message: {
+    content: string
+    author: { bot: boolean }
+    react: (arg0: string) => void
+  }) {
+    if (message.author.bot) return
+
     const content = message.content.replace(
       '<:ribeiro3:855147982110457877>',
       '',
     )
+
     if (
       keywords.some((word) => content.toLowerCase().includes(word)) &&
       Math.random() <= 0.326 // 32.5% chance of reacting
