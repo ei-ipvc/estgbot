f76939b8f7475b59d8f450ebeff38a29de9fe10f
Matthew Rodrigues <matt@capaz.dev>
2024-06-15 16:37:37 +0100

    feat: init cmd cooldown logic

--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -6,21 +6,40 @@ const err = {
   ephemeral: true,
 }
 
+const cdMsgs = [
+  'Espera um pouco antes de executares outro comando! <:feelsWeak:1192128377486852130>',
+  'Não exageres nos comandos! <:burCat:1232019409816649768>',
+]
+
 module.exports = {
   name: Events.InteractionCreate,
   async execute(interaction: {
-    isChatInputCommand: () => any
     client: { cmds: { get: (arg0: any) => any } }
     commandName: string
-    replied: any
     deferred: any
+    replied: any
+    user: { lastCommand: number }
+
+    deferReply: (arg0: { ephemeral: boolean }) => any
     followUp: (arg0: { content: string; ephemeral: boolean }) => any
+    isChatInputCommand: () => any
     reply: (arg0: { content: string; ephemeral: boolean }) => any
   }) {
     if (!interaction.isChatInputCommand()) return
 
     const cmd = interaction.client.cmds.get(interaction.commandName)
     try {
+      if (
+        interaction.user.lastCommand &&
+        interaction.user.lastCommand + 2000 > Date.now()
+      ) {
+        return interaction.reply({
+          content:
+            cdMsgs[Math.floor(Math.random() * cdMsgs.length)] || cdMsgs[0],
+          ephemeral: true,
+        })
+      } else interaction.user.lastCommand = Date.now()
+
       await cmd.execute(interaction)
     } catch (error) {
       console.error(error)
