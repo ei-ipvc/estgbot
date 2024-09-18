bf7579ff52611dd9aad66dac32356b08dc656e1e
Matthew Rodrigues <mattcapazz@gmail.com>
2024-09-18 23:11:57 +0100

    chore(guildMemberAdd): added more reactions when user joins

--- events/guildMemberAdd.ts
+++ events/guildMemberAdd.ts
@@ -1,6 +1,6 @@
 import { Events, GuildMember, TextChannel } from 'discord.js'
 import { client } from '../index'
-import { pogEmoji, updtMemberCountActivity } from '../global'
+import { updtMemberCountActivity } from '../global'
 
 module.exports = {
   name: Events.GuildMemberAdd,
@@ -14,8 +14,28 @@ module.exports = {
       `Boas ${member}! Sê bem-vindo ao discord de Engenharia Informática do IPVC 👋`,
     )
 
-    const msg = await channel.messages.fetch({ limit: 1 })
-    msg.first()?.react(pogEmoji())
+    const emojis = [
+      '<:catHappy:1285662157316427796>',
+      '<:pepeOkay:1285662139033321585>',
+      '<:peepoHappy:1285662121375432795>',
+      '<a:peepoParty:1285662104317067324>',
+      '<:lookinLeftPog:1285662088542290035>',
+      '<:peepoSmile:1285662066992087082>',
+      '<:pepeCool:1285662044980514907>',
+      '<:peepoCool:1285662026559000647>',
+      '<:poggies:1285662010326913065>',
+      '<a:peepoSalute:1285661992090075217>',
+      '<:peepoPog:1285661954731544647>',
+    ]
+
+    channel.messages
+      .fetch({ limit: 5 })
+      .then((msgs) => {
+        const botMsg = msgs.find((msg) => msg.author.bot)
+        if (botMsg)
+          botMsg.react(emojis[Math.floor(Math.random() * emojis.length)])
+      })
+      .catch(console.error)
 
     updtMemberCountActivity(client)
   },
